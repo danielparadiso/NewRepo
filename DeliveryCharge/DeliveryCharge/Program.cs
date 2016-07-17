e182bc60a08b0576a6b5fcd7a851ba18ac0c9e6e
danielparadiso <d_piadiso@hotmail.com>
2016-07-17 15:07:11 -0400

    It works

--- DeliveryCharge/DeliveryCharge/Program.cs
+++ DeliveryCharge/DeliveryCharge/Program.cs
@@ -1,20 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeliveryCharge
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            int[] zipCode = { 12321, 23432, 34543, 45654, 56765, 67876, 78987, 89098, 90109, 01210 };
+            int[] zipCode = {12321, 23432, 34543, 45654, 56765, 67876, 78987, 89098, 90109, 01210};
             double[] deliverCharge = {1.00, 2.50, 3.75, 4.25, 5.50, 6.25, 7.30, 8.90, 9.45, 10.25};
-            int inputString = 0;
-            Console.WriteLine("Enter a zipcode (or enter 999 to quit): ");
+            var inputString = "";
+            double charge = 0;
+            var input = 0;
+            var isValidZip = false;
+            int x;
+            Console.WriteLine("Enter a zip code (or enter 999 to quit): ");
+            inputString = Console.ReadLine();
+            input = Convert.ToInt32(inputString);
+            x = 0;
+            while (input != 999)
+            {
+                while (x < zipCode.Length && input != zipCode[x])
+                    ++x;
+                if (x != zipCode.Length)
+                {
+                    isValidZip = true;
+                    charge = deliverCharge[x];
+                }
+                if (isValidZip)
+                    Console.WriteLine("The delivery charge for {0} is {1}.", zipCode[x], charge.ToString("C"));
+                else
+                    Console.WriteLine("We do not deliver to that zip code.");
 
+                isValidZip = false;
+                x = 0;
+                Console.WriteLine("Enter a zip code (or enter 999 to quit): ");
+                inputString = Console.ReadLine();
+                input = Convert.ToInt32(inputString);
+            }
+            Console.WriteLine("Have a nice day!");
+            Console.ReadKey();
         }
     }
 }
