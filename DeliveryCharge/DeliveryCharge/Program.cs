e241655d7dcf12875ed44eba81f6960aecd77a36
danielparadiso <d_piadiso@hotmail.com>
2016-07-17 15:19:20 -0400

    Fixed it so the zip codes are strings, not ints

--- DeliveryCharge/DeliveryCharge/Program.cs
+++ DeliveryCharge/DeliveryCharge/Program.cs
@@ -6,20 +6,18 @@ namespace DeliveryCharge
     {
         private static void Main(string[] args)
         {
-            int[] zipCode = {12321, 23432, 34543, 45654, 56765, 67876, 78987, 89098, 90109, 01210};
+            string[] zipCode = {"12321", "23432", "34543", "45654", "56765", "67876", "78987", "89098", "90109", "01210"};
             double[] deliverCharge = {1.00, 2.50, 3.75, 4.25, 5.50, 6.25, 7.30, 8.90, 9.45, 10.25};
             var inputString = "";
             double charge = 0;
-            var input = 0;
             var isValidZip = false;
             int x;
             Console.WriteLine("Enter a zip code (or enter 999 to quit): ");
             inputString = Console.ReadLine();
-            input = Convert.ToInt32(inputString);
             x = 0;
-            while (input != 999)
+            while (inputString != "999")
             {
-                while (x < zipCode.Length && input != zipCode[x])
+                while (x < zipCode.Length && inputString != zipCode[x])
                     ++x;
                 if (x != zipCode.Length)
                 {
@@ -29,13 +27,13 @@ namespace DeliveryCharge
                 if (isValidZip)
                     Console.WriteLine("The delivery charge for {0} is {1}.", zipCode[x], charge.ToString("C"));
                 else
-                    Console.WriteLine("We do not deliver to that zip code.");
+                    Console.WriteLine("We do not deliver to {0}.", inputString);
 
                 isValidZip = false;
                 x = 0;
                 Console.WriteLine("Enter a zip code (or enter 999 to quit): ");
                 inputString = Console.ReadLine();
-                input = Convert.ToInt32(inputString);
+
             }
             Console.WriteLine("Have a nice day!");
             Console.ReadKey();
