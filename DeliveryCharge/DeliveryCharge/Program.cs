89bf6aa26f34ac88c85863cc190679be25fc4366
danielparadiso <d_piadiso@hotmail.com>
2016-07-17 14:10:54 -0400

    First commit to see if I'm doing this right

---
+++ DeliveryCharge/DeliveryCharge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryCharge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zipCode = { 12321, 23432, 34543, 45654, 56765, 67876, 78987, 89098, 90109, 01210 };
+            double[] deliverCharge = {1.00, 2.50, 3.75, 4.25, 5.50, 6.25, 7.30, 8.90, 9.45, 10.25};
+            int inputString = 0;
+            Console.WriteLine("Enter a zipcode (or enter 999 to quit): ");
+
+        }
+    }
+}
