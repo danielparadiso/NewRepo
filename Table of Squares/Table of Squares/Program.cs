0272bc51db86f182aa749dab033c3a5bf46a874f
danielparadiso <d_piadiso@hotmail.com>
2016-07-26 19:56:52 -0400

    A formatted table that shows numbers 1-20 and their squares

---
+++ Table of Squares/Table of Squares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableOfSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("  Table of Squares");
+            for (int i = 1; i <= 20; i++)
+                Console.Write("-");
+            Console.WriteLine();
+            for (int i = 1; i <= 20; i++)
+            {
+                double sum;
+                sum = Math.Pow(i, 2);
+                Console.WriteLine("    {0} ^ 2 = {1}", i, sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
