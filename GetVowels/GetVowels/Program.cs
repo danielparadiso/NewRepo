2b8f2c029c73ddf9b04aef4be156c53f176b8df3
danielparadiso <d_piadiso@hotmail.com>
2016-07-26 19:43:44 -0400

    A program that checks to see if user input is a vowel. Loops until user quits

---
+++ GetVowels/GetVowels/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter = "";
+            char l;
+            Console.WriteLine("Enter a vowel (or ! to quit): ");
+            letter = Console.ReadLine();
+            l = Convert.ToChar((letter));
+            while (l != '!')
+            {
+                if (l == 'A' || l == 'a' || l == 'E' || l == 'e' || l == 'I' || l == 'i' || l == 'O' || l == 'o' ||
+                    l == 'U' || l == 'u')
+                    Console.WriteLine("OK");
+                else
+                    Console.WriteLine("That is not a vowel. Try again.");
+                Console.WriteLine("Enter a vowel (or ! to quit): ");
+                letter = Console.ReadLine();
+                l = Convert.ToChar((letter));
+            }
+            Console.ReadLine();
+        }
+    }
+}
