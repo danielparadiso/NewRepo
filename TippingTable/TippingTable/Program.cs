0bd512bfa918407c47573e50b211948505e88a61
danielparadiso <d_piadiso@hotmail.com>
2016-07-26 20:11:43 -0400

    A table that shows the proper amount of money to tip for a certain dinner price

---
+++ TippingTable/TippingTable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tipping
+{
+    public class TippingTable
+    {
+        private static void Main(string[] args)
+        {
+            var dinnerPrice = 10.00;
+            double tipRate;
+            double tip;
+            const double LOWRATE = 0.10;
+            const double MAXRATE = 0.25;
+            const double TIPSTEP = 0.05;
+            const double MAXDINNER = 100.00;
+            const double DINNERSTEP = 10.00;
+            Console.Write("   Price");
+            for (tipRate = LOWRATE; tipRate <= MAXRATE; tipRate += TIPSTEP)
+                Console.Write("{0, 8}", tipRate.ToString("P"));
+            Console.WriteLine();
+            const int NUM_DASHES = 40;
+            for (var x = 0; x <= NUM_DASHES; ++x)
+                Console.Write("-");
+            Console.WriteLine();
+            tipRate = LOWRATE;
+            while (dinnerPrice <= MAXDINNER)
+            {
+                Console.Write("{0, 8}", dinnerPrice.ToString("C"));
+                while (tipRate <= MAXRATE)
+                {
+                    tip = dinnerPrice*tipRate;
+                    Console.Write("{0, 8}", tip.ToString("C"));
+                    tipRate += TIPSTEP;
+                }
+                dinnerPrice += DINNERSTEP;
+                tipRate = LOWRATE;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
