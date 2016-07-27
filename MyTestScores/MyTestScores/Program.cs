2d6cf3b9849512014820ead15f256c7615f04c51
danielparadiso <d_piadiso@hotmail.com>
2016-07-26 20:19:09 -0400

    An array manipulation program

---
+++ MyTestScores/MyTestScores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyTestScores
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var scores = new int[8];
+            int x;
+            string inputString;
+            const string FORMAT = "{0, 9}";
+
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Enter your score on test {0}", i + 1);
+                inputString = Console.ReadLine();
+                scores[i] = Convert.ToInt32(inputString);
+            }
+
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("Scores in original order:");
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.Write(FORMAT, scores[i]);
+            }
+
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("Scores in sorted order:");
+            Array.Sort(scores);
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.Write(FORMAT, scores[i]);
+            }
+
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("Scores in reverse order:");
+            Array.Reverse(scores);
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.Write(FORMAT, scores[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
